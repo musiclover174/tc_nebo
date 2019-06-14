4614ec0ffa2ebe9fe295540e346b6daca225de19
Nikita Kuvykin <arrayofchar@inbox.ru>
2019-06-14 08:21:51 +0500

    - update

--- dev/js/building/modules/news.js
+++ dev/js/building/modules/news.js
@@ -1,3 +1,5 @@
+import { qs } from './helpers';
+
 export default class News {
   constructor(galleryEl, thumbsEl) {
     this.galleryEl = galleryEl;
@@ -7,31 +9,42 @@ export default class News {
   }
 
   init() {
-    const galleryThumbs = new Swiper(this.thumbsEl, {
-      spaceBetween: 18,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
-      breakpoints: {
-        500: {
-          slidesPerView: 2,
+    if (qs(this.thumbsEl)) {
+      const galleryThumbs = new Swiper(this.thumbsEl, {
+        spaceBetween: 18,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+          500: {
+            slidesPerView: 2,
+          },
+          767: {
+            slidesPerView: 3,
+          },
         },
-        767: {
-          slidesPerView: 3,
+      });
+      const galleryTop = new Swiper(this.galleryEl, {
+        spaceBetween: 10,
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.js-news-next',
+          prevEl: '.js-news-prev',
         },
-      },
-    });
-    const galleryTop = new Swiper(this.galleryEl, {
-      spaceBetween: 10,
-      slidesPerView: 1,
-      navigation: {
-        nextEl: '.js-news-next',
-        prevEl: '.js-news-prev',
-      },
-      thumbs: {
-        swiper: galleryThumbs,
-      },
-    });
+        thumbs: {
+          swiper: galleryThumbs,
+        },
+      });
+    } else {
+      const galleryTop = new Swiper(this.galleryEl, {
+        spaceBetween: 10,
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.js-news-next',
+          prevEl: '.js-news-prev',
+        },
+      });
+    }
   }
 }
